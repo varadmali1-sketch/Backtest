2e57a3de6efdc3906289959120a10258430d41a1
varadmali1-sketch <varadmali1@gmail.com>
2026-04-13 23:44:19 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Working 🚀");
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
